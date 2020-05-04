a3ebb3c7414e0c1f1e7d33ed055fec3e5a9dc072
impleman <impleman@example.com>
2020-05-04 21:35:14 +0800

    without css

--- java/shareit-switch/src/main/java/com/sjtuopennetwork/shareit/ShareService.java
+++ java/shareit-switch/src/main/java/com/sjtuopennetwork/shareit/ShareService.java
@@ -1,6 +1,9 @@
 package com.sjtuopennetwork.shareit;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
 
 import com.huawei.hilink.openapi.nativelauncher.LaunchedProcess;
 import com.huawei.hilink.openapi.nativelauncher.NativeLauncher;
@@ -21,15 +24,53 @@ public class ShareService extends AbstractRESTResource {
     // global vars
     String usbPath;
     String homeDir;
+    LinkedList<String> testWhiteList=new LinkedList<>((Collection<String>)Arrays.asList(new String[]{"p111","p222"}));
 
     public ShareService(){
         super("shareit");
         System.out.println(TAG+"restservice start");
+        buildDescriptor("/shareit/getWhiteList", HTTP_GET, null, "getWhiteList");
         buildDescriptor("/shareit/postCmd", HTTP_POST, PostData.class, "postCmd");
     }
 
+    class ResponseData{
+        String responseType;
+        String[] peerIdList;
+        public ResponseData(String r, String[] p){
+            responseType=r;
+            peerIdList=p;
+        }
+    }
+
+    public Response getWhiteList(){
+        String responseType="whiteList";
+        String[] w=(String[])testWhiteList.toArray(new String[0]);
+        for(String s:w){
+            System.out.println(TAG+"res: "+s);
+        }
+        ResponseData responseData=new ResponseData(responseType, w);
+        return Response.ok(responseData).build();
+    }
+
     public Response postCmd(PostData postData){
-        System.out.println(TAG+"get Cmd:"+postData.toString());
+        switch(postData.cmdType){
+            case "addWhiteList":
+                // add a peerid to whiteList
+                System.out.println(TAG+"add:"+postData.cmd);
+                testWhiteList.add(postData.cmd);
+                break;
+            case "delWhiteList":
+                System.out.println(TAG+"del"+postData.cmd);
+                for(int i=0;i<testWhiteList.size();i++){
+                    if(testWhiteList.get(i).equals(postData.cmd)){
+                        testWhiteList.remove(i);
+                        break;
+                    }
+                }
+            default:
+                System.out.println(TAG+"get Cmd:"+postData.toString());
+        }
+
         return Response.ok().build();
     }
 
@@ -41,16 +82,18 @@ public class ShareService extends AbstractRESTResource {
         if(!homeDirFile.exists()){
             homeDirFile.mkdir();
             System.out.println(TAG+"shareit dir created");
+        }else{
+            System.out.println(TAG+"shareit dir exists");
         }
 
         // launch the native progress
-        // textile=nativeLauncher.launch("testbin.txt", new String[]{});
-
+        // textile=nativeLauncher.launch("shadow", new String[]{"init",homeDir+"/test1"});
+        System.out.println(TAG+"init textile");
     }
 
-
     protected void deactivate() {
-        // nativeLauncher.stop(textile);
+        nativeLauncher.stop(textile);
+        System.out.println(TAG+"stop textile");
     }
 
     //=== services binder ================================================
