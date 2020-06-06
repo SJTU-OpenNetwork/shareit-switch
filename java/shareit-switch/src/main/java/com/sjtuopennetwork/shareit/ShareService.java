01f7747464806ad72bb0729fdbfec68842bed2e6
impleman <impleman@example.com>
2020-06-06 20:29:11 +0800

    connect switch

--- java/shareit-switch/src/main/java/com/sjtuopennetwork/shareit/ShareService.java
+++ java/shareit-switch/src/main/java/com/sjtuopennetwork/shareit/ShareService.java
@@ -1,6 +1,8 @@
 package com.sjtuopennetwork.shareit;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.LinkedList;
 
 import com.huawei.hilink.openapi.nativelauncher.LaunchedProcess;
@@ -10,7 +12,7 @@ import com.huawei.hilink.rest.AbstractRESTResource;
 import com.huawei.hilink.rest.Response;
 
 public class ShareService extends AbstractRESTResource {
-    public static String TAG="==================== ";
+    public static String TAG = "==================== ";
 
     // services
     private USBStorage usbStorage = null;
@@ -22,65 +24,85 @@ public class ShareService extends AbstractRESTResource {
     // global vars
     String usbPath;
     String homeDir;
-    // LinkedList<String> testWhiteList=new LinkedList<>((Collection<String>)Arrays.asList(new String[]{"p111","p222"}));
-    LinkedList<String> testWhiteList=new LinkedList<>();
-    boolean nodeStart=false;
+    ResponseAddr responseAddr;
+    // LinkedList<String> testWhiteList=new
+    // LinkedList<>((Collection<String>)Arrays.asList(new String[]{"p111","p222"}));
+    LinkedList<String> testWhiteList = new LinkedList<>();
+    boolean nodeStart = false;
 
-    public ShareService(){
+    public ShareService() {
         super("shareit");
-        System.out.println(TAG+"restservice start");
+        System.out.println(TAG + "restservice start");
         buildDescriptor("/shareit/getWhiteList", HTTP_GET, null, "getWhiteList");
+        buildDescriptor("/shareit/startNode", HTTP_GET, null, "startNode");
         buildDescriptor("/shareit/postCmd", HTTP_POST, PostData.class, "postCmd");
     }
 
-    class ResponseData{
+    class ResponseData {
         String responseType;
         String[] peerIdList;
-        public ResponseData(String r, String[] p){
-            responseType=r;
-            peerIdList=p;
+
+        public ResponseData(String r, String[] p) {
+            responseType = r;
+            peerIdList = p;
         }
     }
 
-    public Response getWhiteList(){
-        String responseType="whiteList";
-        String[] w=(String[])testWhiteList.toArray(new String[0]);
-        for(String s:w){
-            System.out.println(TAG+"res: "+s);
+    class ResponseAddr{
+        String responseType;
+        String addr;
+        public ResponseAddr(String s1,String s2){
+            responseType=s1;
+            addr=s2;
         }
-        ResponseData responseData=new ResponseData(responseType, w);
+    }
+
+    public Response startNode() {
+        // 从文件中读取，第二行
+        if(!nodeStart){
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(homeDir + "/shadow/address"));
+                bufferedReader.readLine();
+                String swarmAddr = bufferedReader.readLine();
+                responseAddr=new ResponseAddr("swarmAddr", swarmAddr);
+                bufferedReader.close();
+                nodeStart=true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(TAG + responseAddr.addr);
+        return Response.ok(responseAddr).build();
+    }
+
+    public Response getWhiteList() {
+        String responseType = "whiteList";
+        String[] w = (String[]) testWhiteList.toArray(new String[0]);
+        for (String s : w) {
+            System.out.println(TAG + "res: " + s);
+        }
+        ResponseData responseData = new ResponseData(responseType, w);
         return Response.ok(responseData).build();
     }
 
-    public Response postCmd(PostData postData){
-        switch(postData.cmdType){
-            case "startNode":
-                if(!nodeStart){
-                    nativeLauncher.launch("shadow-arm",new String[]{"start",homeDir+"/shadow"});
-                    System.out.println(TAG+"start textile");
-                    nodeStart=true;
-                    // testWhiteList.add("aaa");
-                    // testWhiteList.add("bbb");
-                }else{
-                    System.out.println(TAG+"node already started");
-                }
-                break;
+    public Response postCmd(PostData postData) {
+        switch (postData.cmdType) {
             case "addWhiteList":
                 // add a peerid to whiteList
-                System.out.println(TAG+"add:"+postData.cmd);
+                System.out.println(TAG + "add:" + postData.cmd);
                 testWhiteList.add(postData.cmd);
-                nativeLauncher.launch("shadow-arm", new String[]{"whitelist","add",postData.cmd});
+                nativeLauncher.launch("shadow-arm", new String[] { "whitelist", "add", postData.cmd });
                 break;
             case "delWhiteList":
-                System.out.println(TAG+"del"+postData.cmd);
-                for(int i=0;i<testWhiteList.size();i++){
-                    if(testWhiteList.get(i).equals(postData.cmd)){
+                System.out.println(TAG + "del" + postData.cmd);
+                for (int i = 0; i < testWhiteList.size(); i++) {
+                    if (testWhiteList.get(i).equals(postData.cmd)) {
                         testWhiteList.remove(i);
                         break;
                     }
                 }
             default:
-                System.out.println(TAG+"get Cmd:"+postData.toString());
+                System.out.println(TAG + "get Cmd:" + postData.toString());
         }
 
         return Response.ok().build();
@@ -88,20 +110,37 @@ public class ShareService extends AbstractRESTResource {
 
     protected void activate() {
         // create shareit directory, and test the write permission
-        usbPath=usbStorage.getDiskVolumes().get(0).path;
-        homeDir=usbPath+"/shareit";
-        System.out.println(TAG+homeDir);
-        File homeDirFile=new File(homeDir);
-        if(!homeDirFile.exists()){
+        usbPath = usbStorage.getDiskVolumes().get(0).path;
+        homeDir = usbPath + "/shareit";
+        System.out.println(TAG + homeDir);
+        File homeDirFile = new File(homeDir);
+        if (!homeDirFile.exists()) {
             homeDirFile.mkdir();
-            System.out.println(TAG+"shareit dir created");
-        }else{
-            System.out.println(TAG+"shareit dir exists");
+            System.out.println(TAG + "shareit dir created");
+        } else {
+            System.out.println(TAG + "shareit dir exists");
         }
 
         // launch the native progress
-        textile=nativeLauncher.launch("shadow-arm", new String[]{"init",homeDir+"/shadow"});
-        System.out.println(TAG+"init textile");
+        textile = nativeLauncher.launch("shadow-arm", new String[] { "init", homeDir + "/shadow" });
+        System.out.println(TAG + "init textile");
+
+        // if(!nodeStart){
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                nativeLauncher.launch("shadow-arm",new String[]{"start",homeDir+"/shadow"});
+                System.out.println(TAG+"start textile");
+            }
+        }.start();
+        // }else{
+        //     System.out.println(TAG+"node already started");
+        // }
     }
 
     protected void deactivate() {
